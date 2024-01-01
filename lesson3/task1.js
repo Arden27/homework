adef84927b70f95d4a817851ad3e875d3be6272d
Artem Furman <artemfurman@gmail.com>
2024-01-01 22:39:17 +0100

    complete lesson3/task1

---
+++ lesson3/task1.js
@@ -0,0 +1,13 @@
+Array.prototype.myFilter = function myFilter(callbackFn, thisArg) {
+  let result = [];
+
+  for (let index = 0; index < this.length; index++) {
+    const item = this[index];
+
+    if (callbackFn.call(thisArg, item, index, this)) {
+      result.push(item);
+    }
+  }
+
+  return result;
+}
