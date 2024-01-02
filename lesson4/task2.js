cbb56082cb0d32f57bc0139ec22480ff474917eb
Artem Furman <artemfurman@gmail.com>
2024-01-02 05:46:50 +0100

    complete lesson4/task2

--- lesson4/task2.js
+++ lesson4/task2.js
@@ -1,17 +1,61 @@
 class Calculator {
-  constructor(num1, num2, ...args) {
-    if (!this.isValidNum(num1) || !this.isValidNum(num2) || args.length > 0) {
-      throw new Error('Конструктор можеть принять только два валидных числа!')
-    }
-  }
+	constructor(x, y, ...args) {
+		if (this.isInvalidInput(x, y, ...args)) {
+			throw new Error('Конструктор ожидает два валидных числа');
+		}
 
-  isValidNum(value) {
-    return typeof value === 'number' && !isNaN(value) && isFinite(value);
-  }
+		this.x = x;
+		this.y = y;
 
-  // invalidInput(num1, num2, ...args) {
+		this.setX = this.setX.bind(this);
+		this.setY = this.setY.bind(this);
+		this.logSum = this.logSum.bind(this);
+		this.logMul = this.logMul.bind(this);
+		this.logSub = this.logSub.bind(this);
+		this.logDiv = this.logDiv.bind(this);
+	}
 
-  // }
+	setX(num) {
+		if (this.isInvalidNum(num)) {
+			throw new Error('Невалидное число');
+		}
+    
+		this.x = num;
+	}
+
+	setY(num) {
+		if (this.isInvalidNum(num)) {
+			throw new Error('Невалидное число');
+		}
+
+		this.y = num;
+	}
+
+	logSum() {
+		console.log(this.x + this.y);
+	}
+
+	logMul() {
+		console.log(this.x * this.y);
+	}
+
+	logSub() {
+		console.log(this.x - this.y);
+	}
+
+	logDiv() {
+		if (this.y === 0) {
+			throw new Error('Нельзя делить на ноль');
+		}
+
+		console.log(this.x / this.y);
+	}
+
+	isInvalidNum(value) {
+		return typeof value !== 'number' || isNaN(value) || !isFinite(value);
+	}
+
+	isInvalidInput(x, y, ...args) {
+		return this.isInvalidNum(x) || this.isInvalidNum(y) || args.length > 0;
+	}
 }
-
-let calc = new Calculator(1, 2, 3)
